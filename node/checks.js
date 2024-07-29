766c43408cfb0e3ad2e6bbd118045c6be36f2566
Coby Benveniste <coby.benveniste@gmail.com>
2024-07-29 08:52:53 +0300

    Update checks

--- node/checks.js
+++ node/checks.js
@@ -4,14 +4,18 @@ async function main(_params) {
   const unsignedTester = [10, 100000, 65999, 10, 10, 0, 1, 1, 2000, 0xffffffff];
   const unsignedSize =
     FastIntegerCompression.computeCompressedSizeInBytes(unsignedTester);
+  const unsignedCompressed = FastIntegerCompression.compress(unsignedTester);
 
   console.log("Unsigned size compressed", unsignedSize);
+  console.log("Unsigned compressed", unsignedCompressed);
 
   const signedTester = [10, 100000, 65999, 10, 10, 0, -1, -1, -2000];
   const signedSize =
     FastIntegerCompression.computeCompressedSizeInBytesSigned(signedTester);
+  const signedCompressed = FastIntegerCompression.compressSigned(signedTester);
 
   console.log("Signed size compressed", signedSize);
+  console.log("Signed compressed", signedCompressed);
 }
 
 const params = {};
