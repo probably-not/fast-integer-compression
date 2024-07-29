fed3727c433c0c44ae8066a8d003744c36bbb873
Coby Benveniste <coby.benveniste@gmail.com>
2024-07-29 07:51:07 +0300

    Add node folder for checks

---
+++ node/checks.js
@@ -0,0 +1,18 @@
+import FastIntegerCompression from "fastintcompression";
+
+async function main(_params) {
+  const size = FastIntegerCompression.computeCompressedSizeInBytes([
+    10, 100000, 65999, 10, 10, 0, 1, 1, 2000, 0xffffffff,
+  ]);
+
+  console.log(size);
+}
+
+params = {};
+main(params)
+  .then(() => {
+    console.log("Completed checks");
+  })
+  .catch((reason) => {
+    console.error("Failed to complete checks", reason);
+  });
