336d768bead9097b498ee88c9490262a971e1a43
Coby Benveniste <coby.benveniste@gmail.com>
2024-07-29 11:09:36 +0300

    Add actions

--- lib/fast_integer_compression/unsigned/decompressor.ex
+++ lib/fast_integer_compression/unsigned/decompressor.ex
@@ -2,7 +2,17 @@ defmodule FastIntegerCompression.Unsigned.Decompressor do
   @moduledoc false
 
   @spec decompress(bitstring()) :: list(non_neg_integer())
-  def decompress(_buf) do
-    []
+  def decompress(buf) do
+    lst = :erlang.bitstring_to_list(buf)
+    size = :erlang.size(buf)
+
+    Enum.reduce_while(lst, {[], 0, size}, &do_decompress/2)
+  end
+
+  defp do_decompress(_val, {lst, pos, size}) when size <= pos do
+    {:halt, lst}
+  end
+
+  defp do_decompress(_val, {_lst, _pos, _size}) do
   end
 end
