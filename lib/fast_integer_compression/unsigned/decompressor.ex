f2cbbd847744a5efa1f8102e401f0c1adf92a73f
Coby Benveniste <coby.benveniste@gmail.com>
2024-08-05 08:27:48 +0300

    Decompress given binary

--- lib/fast_integer_compression/unsigned/decompressor.ex
+++ lib/fast_integer_compression/unsigned/decompressor.ex
@@ -1,18 +1,49 @@
 defmodule FastIntegerCompression.Unsigned.Decompressor do
   @moduledoc false
 
+  import Bitwise
+
   @spec decompress(binary()) :: list(non_neg_integer())
   def decompress(buf) do
-    lst = :erlang.bitstring_to_list(buf)
-    size = :erlang.size(buf)
-
-    Enum.reduce_while(lst, {[], 0, size}, &do_decompress/2)
+    pos = 0
+    size = byte_size(buf)
+    acc = []
+    do_decompress(buf, pos, size, acc)
   end
 
-  defp do_decompress(_val, {lst, pos, size}) when size <= pos do
-    {:halt, lst}
+  defp do_decompress(_buf, pos, size, acc) when pos >= size, do: Enum.reverse(acc)
+
+  defp do_decompress(buf, pos, size, acc) do
+    <<_already_decoded::binary-size(pos), c::signed-integer-8, _rest::binary>> = buf
+    {v, new_pos} = decode_value(buf, pos, band(c, 0x7F))
+    do_decompress(buf, new_pos, size, [v | acc])
   end
 
-  defp do_decompress(_val, {_lst, _pos, _size}) do
+  defp decode_value(buf, pos, v) do
+    <<_already_decoded::binary-size(pos), c::signed-integer-8, _rest::binary>> = buf
+
+    if c >= 0 do
+      {v, pos + 1}
+    else
+      decode_value_loop(buf, pos + 1, v, 7)
+    end
+  end
+
+  defp decode_value_loop(buf, pos, prev, 28) do
+    <<_already_decoded::binary-size(pos), c::signed-integer-8, _rest::binary>> = buf
+    v = bor(prev, c <<< 28)
+    v = v >>> 0
+    {v, pos + 1}
+  end
+
+  defp decode_value_loop(buf, pos, prev, shift) do
+    <<_already_decoded::binary-size(pos), c::signed-integer-8, _rest::binary>> = buf
+    v = bor(prev, band(c, 0x7F) <<< shift)
+
+    if c >= 0 do
+      {v, pos + 1}
+    else
+      decode_value_loop(buf, pos + 1, v, shift + 7)
+    end
   end
 end
