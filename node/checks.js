f807a1ed35b0198dd9d70457ad0de28e20207588
Coby Benveniste <coby.benveniste@gmail.com>
2024-08-05 08:13:49 +0300

    Add more compatibility checks to the node script

--- node/checks.js
+++ node/checks.js
@@ -5,17 +5,31 @@ async function main(_params) {
   const unsignedSize =
     FastIntegerCompression.computeCompressedSizeInBytes(unsignedTester);
   const unsignedCompressed = FastIntegerCompression.compress(unsignedTester);
+  const unsignedDecompressed =
+    FastIntegerCompression.uncompress(unsignedCompressed);
 
   console.log("Unsigned size compressed", unsignedSize);
   console.log("Unsigned compressed", unsignedCompressed);
+  console.log("Unsigned decompressed", unsignedDecompressed);
+  console.log(
+    "Unsigned Encoded",
+    Buffer.from(unsignedCompressed).toString("base64")
+  );
 
   const signedTester = [10, 100000, 65999, 10, 10, 0, -1, -1, -2000];
   const signedSize =
     FastIntegerCompression.computeCompressedSizeInBytesSigned(signedTester);
   const signedCompressed = FastIntegerCompression.compressSigned(signedTester);
+  const signedDecompressed =
+    FastIntegerCompression.uncompressSigned(signedCompressed);
 
   console.log("Signed size compressed", signedSize);
   console.log("Signed compressed", signedCompressed);
+  console.log("Signed decompressed", signedDecompressed);
+  console.log(
+    "Signed Encoded",
+    Buffer.from(signedCompressed).toString("base64")
+  );
 }
 
 const params = {};
