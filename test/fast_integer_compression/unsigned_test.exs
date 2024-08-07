dea651d33c08843ed1dd676b2aec7981c815392f
Coby Benveniste <coby.benveniste@gmail.com>
2024-08-07 10:56:16 +0300

    Add sort of fuzzing

--- test/fast_integer_compression/unsigned_test.exs
+++ test/fast_integer_compression/unsigned_test.exs
@@ -31,6 +31,19 @@ defmodule FastIntegerCompressionTest.UnsignedTest do
     original = [10, 100_000, 65_999, 10, 10, 0, 1, 1, 2000, 0xFFFFFFFF]
     compressed = Unsigned.compress(original)
     decompressed = Unsigned.decompress(compressed)
-    assert match?(original, decompressed)
+    assert ^original = decompressed
+  end
+
+  test "fuzzish" do
+    count = :rand.uniform(1000)
+    original = Stream.repeatedly(fn -> System.unique_integer([:positive]) end) |> Enum.take(count)
+
+    expected_size = Unsigned.expected_compressed_size(original)
+    compressed = Unsigned.compress(original)
+    decompressed = Unsigned.decompress(compressed)
+
+    assert byte_size(compressed) == expected_size
+    assert FastIntegerCompression.expected_number_of_integers(compressed) == count
+    assert ^original = decompressed
   end
 end
