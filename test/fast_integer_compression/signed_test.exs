da4a1906a8bf92be7c956d0bb0406c0a0771e2a5
Coby Benveniste <coby.benveniste@gmail.com>
2024-08-07 11:01:23 +0300

    Add the same tests to the signed testing

--- test/fast_integer_compression/signed_test.exs
+++ test/fast_integer_compression/signed_test.exs
@@ -8,6 +8,36 @@ defmodule FastIntegerCompressionTest.SignedTest do
   end
 
   test "expected_compressed_size" do
-    assert Signed.expected_compressed_size([10, 100_000, 65_999, 10, 10, 0, -1, -1, -2000]) == 14
+    original = [10, 100_000, 65_999, 10, 10, 0, -1, -1, -2000]
+    assert Signed.expected_compressed_size(original) == 14
+  end
+
+  test "compression" do
+    compressed = Signed.compress([10, 100_000, 65_999, 10, 10, 0, -1, -1, -2000])
+    assert byte_size(compressed) == 14
+  end
+
+  test "decompression" do
+    original = [10, 100_000, 65_999, 10, 10, 0, -1, -1, -2000]
+    compressed = Signed.compress(original)
+    decompressed = Signed.decompress(compressed)
+    assert ^original = decompressed
+  end
+
+  for i <- 1..50//1 do
+    test "fuzzish_#{i}" do
+      count = :rand.uniform(1000)
+
+      original =
+        Stream.repeatedly(fn -> System.unique_integer([]) end) |> Enum.take(count)
+
+      expected_size = Signed.expected_compressed_size(original)
+      compressed = Signed.compress(original)
+      decompressed = Signed.decompress(compressed)
+
+      assert byte_size(compressed) == expected_size
+      assert FastIntegerCompression.expected_number_of_integers(compressed) == count
+      assert ^original = decompressed
+    end
   end
 end
