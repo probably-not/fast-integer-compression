707b17186aaba34418d1310e6ec8816bb730ab3b
Coby Benveniste <coby.benveniste@gmail.com>
2024-08-07 10:59:58 +0300

    Add some formatting to the test

--- test/fast_integer_compression/unsigned_test.exs
+++ test/fast_integer_compression/unsigned_test.exs
@@ -8,18 +8,8 @@ defmodule FastIntegerCompressionTest.UnsignedTest do
   end
 
   test "expected_compressed_size" do
-    assert Unsigned.expected_compressed_size([
-             10,
-             100_000,
-             65_999,
-             10,
-             10,
-             0,
-             1,
-             1,
-             2000,
-             0xFFFFFFFF
-           ]) == 19
+    original = [10, 100_000, 65_999, 10, 10, 0, 1, 1, 2000, 0xFFFFFFFF]
+    assert Unsigned.expected_compressed_size(original) == 19
   end
 
   test "compression" do
